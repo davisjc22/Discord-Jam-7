1196fe79387c14705dd662ba8efa2ff3be5386ac
Connor Davis <davisjc22@gmail.com>
2022-07-23 00:15:36 -0400

    Add new player controller and remove old ones

---
+++ COMBO/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Animator anim;
+    private CharacterController controller;
+    private Vector3 playerVelocity;
+    private bool groundedPlayer;
+    private float playerSpeed = 10.0f;
+    private float jumpHeight = 2.0f;
+    private float gravityValue = -9.81f;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+
+    public LayerMask groundMask;
+    bool isGrounded;
+
+    private void Start()
+    {
+        anim = GetComponentInChildren<Animator>();
+        controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {   
+        groundedPlayer = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        //groundedPlayer = controller.isGrounded;
+        if(groundedPlayer)
+        {
+            Debug.Log("GROUNDED");
+        }
+        if (groundedPlayer && playerVelocity.y < 0)
+        {
+            playerVelocity.y = -2f;
+        }
+
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        controller.Move(move * Time.deltaTime * playerSpeed);
+
+        if (move != Vector3.zero)
+        {
+            gameObject.transform.forward = move;
+        }
+
+        // Changes the height position of the player..
+        if (Input.GetButtonDown("Jump") && groundedPlayer)
+        {
+            Debug.Log("JUMP");
+            playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
+        }
+
+        playerVelocity.y += gravityValue * Time.deltaTime;
+        controller.Move(playerVelocity * Time.deltaTime);
+
+        if(move.magnitude >= 0.1f && groundedPlayer)
+        {
+            anim.SetInteger ("AnimationPar", 1);
+        }
+        else
+        {
+            anim.SetInteger ("AnimationPar", 0);
+        }
+
+    }
+}
