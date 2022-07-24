f80cb50b8c92bebe070a039690ff7c7762768016
Connor Davis <davisjc22@gmail.com>
2022-07-23 20:00:48 -0400

    Update game manager script with ink progress bar

--- COMBO/Assets/Scripts/GameManager.cs
+++ COMBO/Assets/Scripts/GameManager.cs
@@ -28,24 +28,37 @@ public class GameManager : MonoBehaviour
     public float holdTime = 5.0f; // 5 seconds
  
 
+    //Slider Bar
+    public GameObject TimerBar;
+    private Vector3 TimerBarScale;
+    private GameObject bar;
+    private Vector3 barStartScale;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        TimerBarScale = TimerBar.transform.localScale;
+        bar = TimerBar.transform.GetChild(1).gameObject;
+        barStartScale = bar.transform.localScale;
+        bar.transform.localScale = new Vector3(0, barStartScale.y, barStartScale.z);
+        Renderer barRenderer = bar.GetComponent<Renderer>();
+        barRenderer.material.color = Color.magenta;
+        resetTimerBar();
+        hideTimerBar();
     }
 
     // Update is called once per frame
     void Update()
     {
-        // when the player presses down i for a set amount of time, drop an ink
-        // if(Input.GetKeyDown(KeyCode.LeftShift))
-        // {
-        //     makeInk();
-        // }
+        Vector3 barPosition = new Vector3(player.transform.position.x, player.transform.position.y + 2f, player.transform.position.z);
+        TimerBar.transform.position = barPosition;
+
         if(Input.GetKeyDown(Key))
         {
             startTime = Time.time;
             Debug.Log("Starting Timer");
+            showTimerBar();
+            animateTimerBar();
         }
         else if (Input.GetKey(Key))
         {
@@ -54,12 +67,19 @@ public class GameManager : MonoBehaviour
                 Debug.Log("It Works Great!");
                 makeInk();
                 startTime = Time.time;
+                LeanTween.cancel(bar);
+                resetTimerBar();
+                animateTimerBar();
             }
         }
         else if(Input.GetKeyUp(Key))
         {
             Debug.Log("Ending Timer");
             startTime = 0;
+            
+            LeanTween.cancel(bar);
+            resetTimerBar();
+            hideTimerBar();
         }
     }
 
@@ -67,7 +87,6 @@ public class GameManager : MonoBehaviour
     {
         Vector3 startPosition = new Vector3(player.transform.position.x + 2f, 2f, player.transform.position.z);
         GameObject droplet = Instantiate(inkPrefab, startPosition, Quaternion.identity);
-        //oplet.GetComponent<Rigidbody>().velocity = player.transform.TransformDirection(Vector3.forward * 5);
         
     }
 
@@ -75,5 +94,25 @@ public class GameManager : MonoBehaviour
     {
         combo++;
         comboText.text = "X" + combo;
+    }
+
+    void resetTimerBar()
+    {
+        LeanTween.moveLocalX(bar, 0f, 0f);
+        LeanTween.scaleX(bar,0f,0f);
+    }
+    void animateTimerBar()
+    {
+        LeanTween.moveLocalX(bar,0,holdTime);
+        LeanTween.scaleX(bar,barStartScale.x,holdTime);
+    }
+    void hideTimerBar()
+    {
+        LeanTween.scale(TimerBar, Vector3.zero, .3f).setEase( LeanTweenType.easeInBack);
+    }
+
+    void showTimerBar()
+    {
+        LeanTween.scale(TimerBar, TimerBarScale, .5f).setEase( LeanTweenType.easeOutBack);;
     }
 }
